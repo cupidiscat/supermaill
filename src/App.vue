49188a22b3b5860ddb5cc8fa71f01d531151ca76
apple <739945014@qq.com>
2021-02-09 15:55:08 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>ceshi</p>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+@import "./assets/css/reset.css";
+
+</style>
